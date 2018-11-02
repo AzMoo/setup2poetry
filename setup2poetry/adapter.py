6882900c7510b73607a8cc349fbc8d87b01bc024
Matt Magin <matt.magin@cmv.com.au>
2018-11-02 10:41:11 +1030

    Add license and fix issues found by linting and pre-commit

--- setup2poetry/adapter.py
+++ setup2poetry/adapter.py
@@ -1,7 +1,6 @@
 import pkg_resources
-import toml
 
-from email import message_from_string
+import toml
 
 
 class PoetryAdapter:
@@ -9,35 +8,37 @@ class PoetryAdapter:
 
     def __init__(self, package):
         self._package = pkg_resources.require(package)[0]
-        metadata = dict(self._package._pkg_info.items())
+        meta = dict(
+            self._package._pkg_info.items()  # pylint: disable=protected-access
+        )
 
         self._toml_dict = {
             'tool.poetry': {
-                'name': metadata['Name'],
-                'version': metadata['Version'],
-                'description': metadata['Summary'],
-                'authors': [metadata['Author']],
-                'license': metadata['License']
+                'name': meta['Name'],
+                'version': meta['Version'],
+                'description': meta['Summary'],
+                'authors': [meta['Author']],
+                'license': meta['License']
             },
             'tool.poetry.dependencies': {
-                x: y for x,y in self._iter_deps(self._dependencies)
+                x: y for x, y in self._iter_deps(self._dependencies)
             },
             'tool.poetry.dev-dependencies': {
-                x: y for x,y in self._iter_deps(self._get_extras())
+                x: y for x, y in self._iter_deps(self._get_extras())
             },
             'build-system': {
                 'requires': ["poetry>=0.12"],
                 'build-backend': "poetry.masonry.api"
             }
         }
-    
+
     def _iter_deps(self, deps):
         for dep in deps:
             try:
                 yield (dep.name, "={}".format(dep.specs[0][1]))
             except IndexError:
                 yield (dep.name, "*")
-        
+
     def _get_extras(self):
         extra_deps = self._package.requires(self._package.extras)
         return [i for i in extra_deps if i not in self._dependencies]
@@ -46,13 +47,13 @@ class PoetryAdapter:
     def _dependencies(self):
         if self._deps is not None:
             return self._deps
-        else:
-            return self._package.requires()
-    
+
+        return self._package.requires()
+
     def writepath(self, filepath):
         with open(filepath, "w") as f:
             self.write(f)
-    
+
     def write(self, f):
         toml.dump(self._toml_dict, f)
 
